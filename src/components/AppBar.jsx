383e1a4262be956ef67ba578c531f987b00ad531
Romil Lodaya <romil.code09@gmail.com>
2024-04-21 01:49:53 +0530

    Icon

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./css/appbar.css";
+import { auth } from "../config/firebase";
 
 const Appbar = ({ auth }) => {
   return (
     <nav className="navbar">
       <div className="nav-wrapper">
-        <Link to={auth ? "/" : "/tracker"} className="left brand-logo">
+        <Link to={auth ? "/tracker" : "/tracker"} className="left brand-logo">
           <img src={"BT.png"} alt="Logo" className="photo" />
         </Link>
       </div>
